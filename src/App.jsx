e1894fb2cf60d55d5ec93cef027677a2ec92178b
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2026-01-09 10:13:56 -0500

    Switch to production URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const ENV = "development";
+const ENV = "production";
 const API_URL =
   ENV === "development"
     ? "http://localhost:8080/api/solicitudes"
